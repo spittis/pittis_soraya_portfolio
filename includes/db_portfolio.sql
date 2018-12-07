36ef68159f89e1c23678d2b1fad29d5cad70fda6
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2018-12-07 18:19:15 -0500

    contact page works

--- includes/db_portfolio.sql
+++ includes/db_portfolio.sql
@@ -3,12 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 07, 2018 at 12:43 AM
+-- Generation Time: Dec 06, 2018 at 11:27 PM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_portfolio`
@@ -82,29 +88,6 @@ INSERT INTO `skills` (`id`, `img_names`, `img_paths`, `img_descs`) VALUES
 (3, 'Branding', 'branding.png', 'Paired with research, my creative thinking is suitable for helping establish brands and position them apart from the rest.'),
 (4, 'Developing', 'develop.png', 'I am proficient in HTML and CSS, and can build a website for your brand.');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `suitpiece`
---
-
-CREATE TABLE `suitpiece` (
-  `id` int(11) NOT NULL,
-  `section` varchar(10) NOT NULL,
-  `modelName` varchar(20) NOT NULL,
-  `modelDetails` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `suitpiece`
---
-
-INSERT INTO `suitpiece` (`id`, `section`, `modelName`, `modelDetails`) VALUES
-(0, 'photograph', 'Photography', 'I am passionate about taking photos and capturing the moment. I have experience in shooting photography for websites and events, and I am also experienced in food styling.'),
-(2, 'design', 'Design', 'Design comes easiest to me. From logo and website design to event invitations and everything in-between, I love working with clients to complete a piece of beautiful work that we are both proud of.'),
-(3, 'branding', 'Branding', 'Paired with research, my creative thinking is suitable for helping establish brands and position them apart from the rest.'),
-(4, 'developing', 'Developing', 'I am proficient in HTML and CSS, and can build a website for your brand.');
-
 --
 -- Indexes for dumped tables
 --
@@ -128,14 +111,6 @@ ALTER TABLE `skills`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `suitpiece`
---
-ALTER TABLE `suitpiece`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `model` (`section`),
-  ADD UNIQUE KEY `modelDesc` (`modelName`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -157,8 +132,6 @@ ALTER TABLE `reel`
 ALTER TABLE `skills`
   MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `suitpiece`
---
-ALTER TABLE `suitpiece`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- includes/sendEmail.php
+++ includes/sendEmail.php
@@ -1,18 +1,18 @@
 <?php 
 
 
-var_dump($_POST); 
-//the fake_mail is just a temporary replacement of mail() function within PHP
-mail($to, $subject, $message, $headers);{
-    echo '==== Emails ====' .PHP_EOL;
-    echo 'Subject: '.$subject.PHP_EOL;
-    echo 'Email To: '.$to.PHP_EOL;
-    echo 'Message: '.$message.PHP_EOL; 
-    echo '==== Emails Ends ==== '.PHP_EOL;
+// var_dump($_POST); 
+// //the fake_mail is just a temporary replacement of mail() function within PHP
+// mail($to, $subject, $message, $headers);{
+//     echo '==== Emails ====' .PHP_EOL;
+//     echo 'Subject: '.$subject.PHP_EOL;
+//     echo 'Email To: '.$to.PHP_EOL;
+//     echo 'Message: '.$message.PHP_EOL; 
+//     echo '==== Emails Ends ==== '.PHP_EOL;
 
-    return true;
+//     return true;
 
-}
+// }
 
 
 // so you don't need everything above, because basically php has a mail function so he is just making a fake one instead.
@@ -29,24 +29,26 @@ function send_email(){
         }
 
 
-    //email validations: checking fields contain certain values
-        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-            echo 'Your email is not valid';
-            exit;
-        }
+
+    // //email validations: checking fields contain certain values
+    //     if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    //         echo 'Your email is not valid';
+    //         exit;
+    //     }
     
 
 
     $to = 'soraya.pittis@hotmail.com';
     $subject = 'This is an email from '.$_POST['name'];
     $message = 'Message Body: ' .$_POST['message'];
-    $headers = 'From: noreply@sorayapittis.com';
-    $headers .= 'Reply-To: ' .$_POST['email'];
+    $headers = 'From: '.$_POST['email'];
+    //$headers .= 'Reply-To: ' .$_POST['email'];
 
     mail($to, $subject, $message, $headers);
     //In your server use the following line instead:
     //mail($to, $subject, $message, $headers);
 
+    header('Location: ../index.html');
 }
 
 send_email();
